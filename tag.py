7392b8540deb3165159536e3634d375575da856e
Michael Ren <michael.ren@mailbox.org>
2018-05-30 01:31:12 -0400

    show learns slicing and order options and last runs editor

--- tag.py
+++ tag.py
@@ -7,14 +7,29 @@ from argparse import ArgumentParser, Namespace
 from collections import OrderedDict
 from re import compile
 from sys import stdout, argv
-from typing import Sequence, Iterator, Optional, Callable, Mapping, Any
+from typing import Sequence, Iterator, Optional, Callable, Mapping, Any, Tuple
 from pathlib import Path
+from os import environ
+from subprocess import run as subprocess_run
+
+
+#TODO: use generate_query everywhere, write remove command, implement editor support in vim:
+#          - get file name, write file name, add file name to tags mentioned if any
+#          - split this into :W to write with timestamp, :T to add file to any tags, :R to remove any tags, :L to list any tags
+#          - property file support
+#          - journal, bookmarks, note-taking, research, todo list
+#          - solve problem of naming, get it in first, decide how to label it later
+#          - additions to a note should be linked as children of a note
+#          - can either write whole note or quote parts of it or nothing at all, like email
+#          - members start from present by default--we always start from the present and work backwards into the past through successive layers of interpretation
 
 
 class Properties:
     DB_FILE = Path("./tags.sqlite")
 
     NOTES_DIRECTORY = Path(".")
+
+    EDITOR = [environ.get("EDITOR") or "vim"]
 
 
 class TagType(Enum):
@@ -47,11 +62,13 @@ def insert_new_key(mapping: dict, key: Any, value: Any) -> None:
 
 def generate_query(
         query: str,
-        dynamic_args: Optional[Mapping[str, Any]]=None
+        dynamic_args: Optional[Mapping[str, Any]]=None,
+        static_args: Optional[Mapping[str, Any]]=None
         ) -> Callable[[Cursor], Iterator[Row]]:
     dynamic_args = dynamic_args or {}
 
     params = {}
+    params.update(static_args or {})
     args = {}
 
     for key, value in dynamic_args.items():
@@ -251,6 +268,33 @@ RECURSIVE_MEMBERS = (
 )
 
 
+def slice_to_limit_offset(slice_: str) -> Tuple[int, int]:
+    if not slice_:
+        raise ValueError("Empty slice")
+    components = slice_.split(":")
+    if len(components) > 2 or len(components) < 1:
+        raise ValueError("Bad slice: '{}'".format(slice_))
+    if components[0]:
+        start = int(components[0])
+        if start < 0:
+            raise ValueError("Negative slice indices are not supported")
+    else:
+        start = 0
+    if len(components) == 1:
+        end = start + 1
+    elif components[1]:
+        end = int(components[1])
+        if end < 0:
+            raise ValueError("Negative slice indices are not supported")
+    else:
+        end = -1
+
+    offset = start
+    limit = end - start
+
+    return limit, offset
+
+
 class Show(Command):
     HEADER = "{}\n---\n"
 
@@ -260,7 +304,8 @@ class Show(Command):
         "select name"
         "    from tags"
         "    where type = {}"
-        "    order by name;"
+        "    order by name {{order}}"
+        "    limit {{limit}} offset {{offset}};"
     ).format(
         TagType.NOTE.value
     )
@@ -270,7 +315,8 @@ class Show(Command):
         "select distinct name"
         "    from members"
         "    where type = {}"
-        "    order by name;"
+        "    order by name {{order}}"
+        "    limit {{limit}} offset {{offset}};"
     ).format(
         RECURSIVE_MEMBERS,
         TagType.NOTE.value
@@ -282,19 +328,49 @@ class Show(Command):
         parser.add_argument(
             "tags", nargs="*", help="The tags to combine, else all"
         )
+        parser.add_argument(
+            "-r",
+            "--range",
+            help="A continuous range of notes to show in Python slice notation"
+        )
+        parser.add_argument(
+            "-b",
+            "--beginning",
+            action="store_true",
+            help="List notes from beginning forward and not present backward"
+        )
         return parser
 
     @classmethod
     def run(cls, cursor: Cursor, arguments: Namespace) -> None:
+        limit_offset = cls.limit_offset(arguments.range)
+        order = "asc" if arguments.beginning else "desc"
         if arguments.tags:
             query = generate_query(
                 cls.NOTES_OF_CATEGORIES,
-                dict(categories=arguments.tags)
+                dict(
+                    categories=arguments.tags,
+                    **limit_offset
+                ),
+                dict(order=order)
             )
         else:
-            query = generate_query(cls.ALL_NOTES)
+            query = generate_query(
+                cls.ALL_NOTES,
+                limit_offset,
+                dict(order=order)
+            )
         for row in query(cursor):
             cls.print(row["name"])
+
+    @classmethod
+    def limit_offset(cls, range_: str) -> Mapping[str, int]:
+        if range_ is not None:
+            limit, offset = slice_to_limit_offset(range_)
+        else:
+            limit = -1
+            offset = 0
+        return dict(limit=limit, offset=offset)
 
     @classmethod
     def print(cls, member: str) -> None:
@@ -346,8 +422,10 @@ class Last(Command):
         else:
             query = generate_query(cls.ALL_NOTES)
         for row in query(cursor):
-            # run EDITOR
-            print(row["name"])
+            subprocess_run(
+                [*Properties.EDITOR, row["name"]],
+                check=True
+            )
 
 
 COMMANDS = OrderedDict(
