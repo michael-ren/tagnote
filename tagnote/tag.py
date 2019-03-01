39725f90ec1cacf4a4d4ab0e82ec4e9fb72fdbde
Michael Ren <michael.ren@mailbox.org>
2019-03-01 17:29:24 -0500

    fix and add license notice for main module and vim plugin, respectively

--- tagnote/tag.py
+++ tagnote/tag.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
 
-"""
-Copyright 2018 Michael Ren
+# Copyright 2019 Michael Ren <michael.ren@mailbox.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
 
 from abc import ABCMeta, abstractmethod
 from argparse import ArgumentParser, Namespace
